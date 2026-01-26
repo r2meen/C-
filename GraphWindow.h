93f5bc3605793b014ff97e5137d62c6bb9f0b890
Rameen <rameenzulfi0@gmail.com>
2026-01-27 00:16:42 +0500

    Create GraphWindow.h

---
+++ GraphWindow.h
@@ -0,0 +1,17 @@
+#ifndef GRAPHWINDOW_H
+#define GRAPHWINDOW_H
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "LinearEquation.h"
+#include "PlotData.h"
+
+class GraphWindow{
+
+
+
+
+    
+}
+
+#endif
